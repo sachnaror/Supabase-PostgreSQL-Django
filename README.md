47570e871d9c61c234b02b0d469bf632195cae0a
Sachin <sachnaror@gmail.com>
2024-07-23 14:10:41 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Supabase-Django
+
+Supabase-Django is a project that demonstrates how to connect a Django application to Supabase's PostgreSQL database.
+
+## Prerequisites
+
+- Python 3.6+
+- Django 3.0+
+- psycopg2-binary
+
+## Setup
+
+1. **Clone the repository**:
+
+   ```bash
+   git clone https://github.com/yourusername/Supabase-Django.git
+   cd Supabase-Django
+
+2. **Create and activate a virtual environment:**
+
+   ```bash
+   python -m venv venv
+   source venv/bin/activate  # On Windows use `venv\Scripts\activate`
+
+3. **Install the required dependencies:**
+   ```bash
+   pip install -r requirements.txt
+
+4. **Update settings.py**
+   with the Supabase PostgreSQL database configuration:
+   Open Supabase-Django/settings.py and update the DATABASES configuration:
+
+```bash
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'HOST': 'aws-34-ap-south-7.pooler.supabase.com',
+            'NAME': 'postme',
+            'USER': 'postgres.eadfvvbutsqgurzq',
+            'PORT': '6973',
+            'PASSWORD': 'iE0f4b9yL54M',
+        }
+    }
+
+5. **Run migrations:**
+
+    ```bash
+    python manage.py migrate
+
+6. **Run the development server:**
+
+    ```bash
+    python manage.py runserver
---
+++ SupabaseDjango/settings.py
@@ -0,0 +1,126 @@
+
+from pathlib import Path
+
+import psycopg2
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-rx8yn3)1!be&u3frd@u^9j*s!*h@q+cd7u*ssyyx_1^-gxr^r!'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'app1',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'SupabaseDjango.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'SupabaseDjango.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'aws-34-ap-south-7.pooler.supabase.com',
+        'NAME': 'postme',
+        'USER': 'postgres.eadfvvbutsqgurzq',
+        'PORT': '6973',
+        'PASSWORD': 'iE0f4b9yL54M',
+    }
+}
+
+# Password validation
+# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/4.2/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = 'static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
